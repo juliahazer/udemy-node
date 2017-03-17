45277d1b2b67b0e2f237a4bbf3e8995a3771dc94
Julia Hazer <juliahazer@yahoo.com>
2017-03-16 19:29:56 -0700

    refactored notes.js and app.js

--- app.js
+++ app.js
@@ -15,7 +15,16 @@ console.log('Yargs: ', argv);
 console.log('Command ', command);
 
 if (command === 'add'){
-	notes.addNote(argv.title, argv.body);
+	var note = notes.addNote(argv.title, argv.body);
+	if (note === undefined){
+		console.log('Note title already in use.');
+	}
+	else {
+		console.log('Note created');
+		console.log('--');
+		console.log(`Title: ${note.title}`);
+		console.log(`Body: ${note.body}`);
+	}
 } else if (command === 'list'){
 	notes.getAll();
 } else if (command === 'read'){
