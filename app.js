b4d61bf346a56c5aa12996f1c2e516ea95766bb2
Julia Hazer <juliahazer@yahoo.com>
2017-03-16 19:00:47 -0700

    practicing with yargs

--- app.js
+++ app.js
@@ -3,22 +3,25 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+const yargs = require('yargs');
 
 const notes = require('./notes.js'); //require file from relative path
 
+const argv = yargs.argv
 var command = process.argv[2];
 
-console.log('Command: ', command);
-console.log(process.argv);
+console.log('Yargs: ', argv);
+//console.log('Process ', process.argv);
+console.log('Command ', command);
 
 if (command === 'add'){
-	console.log('Adding new note');
+	notes.addNote(argv.title, argv.body);
 } else if (command === 'list'){
-	console.log('Listing all notes');
+	notes.getAll();
 } else if (command === 'read'){
-	console.log('Reading note');
+	notes.getNote(argv.title);
 } else if (command === 'remove'){
-	console.log('Removing note');
+	notes.removeNote(argv.title);
 } else {
 	console.log('Command not recognized');
 }
--- notes.js
+++ notes.js
@@ -1,11 +1,34 @@
 console.log('Starting notes.js');
 
+var addNote = (title, body) => {
+	console.log('Adding note', title, body);
+};
+
+var getAll = () => {
+	console.log('Getting all notes');
+};
+
+var getNote = (title) => {
+	console.log('Getting note ', title);
+};
+
+var removeNote = (title) => {
+	console.log('Removing note ', title);
+};
+
+module.exports = {
+	addNote,
+	getAll,
+	getNote,
+	removeNote
+};
+
 // module.exports.age = 37;
 
-module.exports.addNote = () => {
-	console.log('addNote');
-	return 'New note';
-};
+// module.exports.addNote = () => {
+// 	console.log('addNote');
+// 	return 'New note';
+// };
 
 // module.exports.add = (a,b) => {
 // 	return a+b;
