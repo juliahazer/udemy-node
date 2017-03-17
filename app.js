2cabf7580b2a8ba0f614c84d153a3716836df652
Julia Hazer <juliahazer@yahoo.com>
2017-03-16 17:38:38 -0700

    practice with lodash

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ console.log('Starting app.');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js'); //require file from relative path
 
-var res = notes.addNote();
-console.log(res);
+var filteredArray = _.uniq([1,8,9,9,5,6,6,7,8]);
+console.log(filteredArray);
 
-var sum = notes.add(9,-2);
-console.log(sum);
+// console.log(_.isString(true));
+// console.log(_.isString('Andrew'));
+
+
+// var res = notes.addNote();
+// console.log(res);
+
+// var sum = notes.add(9,-2);
+// console.log(sum);
 
 // var user = os.userInfo();
 
