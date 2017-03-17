04df59e2bb8273c458236eed9cd45fd0914408b9
Julia Hazer <juliahazer@yahoo.com>
2017-03-16 18:50:50 -0700

    testing process.argv to get command line arguments

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ console.log('Starting app.');
 const fs = require('fs');
 const os = require('os');
 const _ = require('lodash');
+
 const notes = require('./notes.js'); //require file from relative path
 
-var filteredArray = _.uniq([1,8,9,9,5,6,6,7,8]);
-console.log(filteredArray);
+var command = process.argv[2];
+
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command === 'add'){
+	console.log('Adding new note');
+} else if (command === 'list'){
+	console.log('Listing all notes');
+} else if (command === 'read'){
+	console.log('Reading note');
+} else if (command === 'remove'){
+	console.log('Removing note');
+} else {
+	console.log('Command not recognized');
+}
+
+// var filteredArray = _.uniq(["julia", "julia", 1,8,9,9,5,6,6,7,8]);
+// console.log(filteredArray);
 
 // console.log(_.isString(true));
 // console.log(_.isString('Andrew'));
